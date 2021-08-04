e4f565ff906492baa5804ef15ad2d8c91e386056
Agustín Curto <agustin.curto@mercadolibre.com>
2021-08-04 18:52:51 -0300

    Update task scheduler model

--- myapp/models.py
+++ myapp/models.py
@@ -1,18 +1,19 @@
+import json
 from django.db import models
 
 class TasksScheduler(models.Model):
     tasks_file = models.FileField(upload_to='files/%Y/%m/%d')
-    raw_tasks = models.CharField(max_length=1000)
-    best_schedule = models.CharField(max_length=1000)
+    raw_tasks = models.JSONField(blank=True, null=True)
+    best_schedule = models.JSONField(blank=True, null=True)
 
     def get_or_create_raw_tasks(self):
         if not self.raw_tasks:
-            # TODO: write code
-            self.raw_tasks = [
-                {"nombre": "foto cliente 1098", "recursos": ["camara", "disco", "proc"] , "ganancia": 9.2},
-                {"nombre": "limpieza de disco", "recursos": ["disco"] , "ganancia": 0.4},
-                {"nombre": "upgrade a v2.1", "recursos": ["proc"] , "ganancia": 2.9}
-            ]
+            self.raw_tasks = []
+            with open(self.tasks_file.path, '+r') as file:
+                json_file = json.loads(file.read())
+                for task in json_file:
+                    self.raw_tasks.append(task)
+            self.save()
         return self.raw_tasks
 
     def get_or_create_best_schedule(self):
@@ -20,4 +21,6 @@ class TasksScheduler(models.Model):
             tasks = self.get_or_create_raw_tasks()
             # TODO: write code
             self.best_schedule = tasks
+            # TODO: end the code
+            self.save()
         return self.best_schedule
